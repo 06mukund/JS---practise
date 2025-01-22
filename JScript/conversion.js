144dbdde3d7a41b86f97e6e059c3388da0697f64
06mukund <115607129+06mukund@users.noreply.github.com>
2025-01-22 11:47:47 +0000

    done

--- JScript/conversion.js
+++ JScript/conversion.js
@@ -10,3 +10,34 @@ console.log(typeof scoreInStr)
 // undefined => number == NaN
 
 // Same way can convert to booelan
+
+// ------------------Operations------------------
+
+// Concatination
+
+console.log(21 + "1");
+console.log("1" + 24);
+console.log("1" + "01");
+console.log(12 + 3 + "3");
+console.log("23" + 3 + 2);
+
+// Post Incremet and Post Increment
+// a++ will first store the number and then print it
+// ++a will first increment the number then print it
+
+let a = 3 
+let b = a++
+
+console.log(a , b);
+//  4 3
+
+let x = 3
+let y = ++x
+console.log(x , y);
+// 4 4
+
+
+
+
+
+
