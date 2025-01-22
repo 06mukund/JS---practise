dc262c25363fb5996287c6d75225fe60b394854b
06mukund <115607129+06mukund@users.noreply.github.com>
2025-01-22 11:20:45 +0000

    done

---
+++ JScript/conversion.js
@@ -0,0 +1,12 @@
+let score = undefined
+
+let scoreInStr = Number(score)
+console.log(scoreInStr);
+console.log(typeof scoreInStr)
+
+// String => Number == NaN
+// Number => String == String
+// null => number == 0
+// undefined => number == NaN
+
+// Same way can convert to booelan
