44c6e8de53d2801f1a39f3f79fe87a6f83b2e15c
06mukund <115607129+06mukund@users.noreply.github.com>
2025-01-26 12:39:51 +0000

    done

---
+++ JScript/num_and_math.js
@@ -0,0 +1,26 @@
+console.log(Math.random())
+
+console.log(Math.ceil(Math.random()));
+console.log(Math.abs(-4));
+
+
+// Ways to generate number between two parameter using random()
+
+max = 20
+min = 10
+console.log(Math.ceil(Math.random() * (max-min + 1) + min));
+
+
+// ----------------------------------------------------------------------------
+
+const score = new Number(123)
+console.log(score)
+//console.log(typeof score);
+
+const rupee = 45.544666
+console.log(rupee.toPrecision(2));
+console.log(rupee.toFixed());
+
+
+
+
