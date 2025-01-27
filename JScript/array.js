361e4ede27dc29609c1873403966620542be513b
06mukund <115607129+06mukund@users.noreply.github.com>
2025-01-27 13:51:38 +0000

    done

---
+++ JScript/array.js
@@ -0,0 +1,27 @@
+let my_array = [1,2,3,45,5]
+//console.log(my_array);
+
+let list = Array(1,2,3,4,5,6,7)
+//console.log(list);
+
+// Common used method of Array - push(), pop(), shift(), unshift(), slice(), splice(), concat(), flat()
+// Important = isArray(), from(), of()
+
+//console.log(my_array.push(list));
+//console.log(...my_array , ...list); // convert to string
+
+// console.log(my_array.slice(1,4)); // Do not add last index element
+// console.log(my_array.splice(1,4)); // Add last index element but this is not exact meaning
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
